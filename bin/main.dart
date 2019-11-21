3f775a1b15117d312cbb3c38dcaf97334ad2ad23
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-21 17:18:01 +0530

    code finished

--- bin/main.dart
+++ bin/main.dart
@@ -4,8 +4,41 @@
 // two strings are anagrams of each other.
 //  Examples
 //  minRemovals("abcde", "cab") ➞ 2
-
-
+int minRemovals(String firstString, String secondString) {
+  List<String> firstList = [];
+  List<String> secondList = [];
+  List<int> indexListA = [];
+  List<String> tempListA = [];
+  List<int> indexListB = [];
+  List<String> tempListB = [];
+  for (int i = 0; i < firstString.length; i++) {
+    firstList.add(firstString[i]);
+  }
+  for (int i = 0; i < secondString.length; i++) {
+    secondList.add(secondString[i]);
+  }
+  for (int i = 0; i < firstList.length; i++) {
+    if (secondList.contains(firstList[i])) {
+      secondList.remove(firstList[i]);
+    } else {
+      indexListA.add(i);
+    }
+  }
+  for (int i = 0; i < indexListA.length; i++) {
+    tempListA.add(firstList[indexListA[i]]);
+  }
+  for (int i = 0; i < secondList.length; i++) {
+    if (tempListA.contains(secondList[i])) {
+      tempListA.remove(secondList[i]);
+    } else {
+      indexListB.add(i);
+    }
+  }
+  for (int i = 0; i < indexListB.length; i++) {
+    tempListB.add(secondList[indexListB[i]]);
+  }
+  return tempListA.length + tempListB.length;
+}
 // Challenge 2
 // Farthest Hamming Anagrams
 // The Hamming distance between two strings is the number of positions at which
@@ -26,6 +59,64 @@
 //  their length (i.e. no letters in the same positions).
 // - false if they aren't anagrams, or
 // - Their Hamming distance if they are anagrams with >=1 letter at the same index.
+hammingDistance(String firstString, String secondString) {
+  if (!(isAnagram(firstString, secondString))) {
+    return false;
+  } else {
+    if (calculateHammingDistance(firstString, secondString) ==
+        firstString.length) {
+      return true;
+    } else {
+      return calculateHammingDistance(firstString, secondString);
+    }
+  }
+}
+
+calculateHammingDistance(String firstString, String secondString) {
+  int hammingCount = 0;
+  //strings are of equal length so calculate hamming distance
+  for (int i = 0; i < firstString.length; i++) {
+    if (firstString[i] != secondString[i]) {
+      hammingCount++;
+    }
+  }
+  return hammingCount;
+}
+
+doesHammingDistanceEqualsLength(String string, int hammingCount) {
+  return string.length == hammingCount;
+}
 
 main() {
+  print(minRemovals("abcde", "abf"));
+  print(minRemovals("abcde", "abc"));
+  print(minRemovals("abcde", "fox"));
+  print(minRemovals("", ""));
+  print(minRemovals("abc", ""));
+  print(hammingDistance("eleven", "tweleve"));
+  print(hammingDistance("read", "dear"));
+  print(hammingDistance("read", "root"));
+  print(hammingDistance("read", "eadr"));
+  print(isAnagram("ballon", "baloon"));
+  print(isAnagram("ballon", "balonl"));
+  print(isAnagram("", ""));
+}
+
+isAnagram(String firstString, String secondString) {
+  List<dynamic> firstStringList = firstString.split('').toList();
+  List<dynamic> secondStringList = secondString.split('').toList();
+  if (firstStringList.length != secondStringList.length) {
+    // if length of the lists is not equal return false
+    return false;
+  } else {
+    for (int i = 0; i < firstStringList.length; i++) {
+      //remove each matching element from the second list
+      if (secondStringList.contains(firstStringList[i])) {
+        //if the strings are anagrams,
+        secondStringList.remove(firstStringList[
+            i]); //the second string list should be empty at the end
+      }
+    }
+  }
+  return secondStringList.isEmpty;
 }
--- test/Day_34_Morning_Challenge_test.dart
+++ test/Day_34_Morning_Challenge_test.dart
@@ -1,4 +1,19 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
+  test('cal point', () {
+    expect(minRemovals("abcde","abc"),2);
+    expect(minRemovals("abcde","abcf"),3);
+    expect(minRemovals("abcde",""),5);
+    expect(minRemovals("","asdf"),4);
+    expect(minRemovals("abcde","fox"),8);
+    expect(minRemovals("",""),0);
+    expect(hammingDistance("eleven","tweleve"),false);
+    expect(hammingDistance("read","dear"),2);
+    expect(hammingDistance("read","root"),false);
+    expect(hammingDistance("read","root"),false);
+    expect(hammingDistance("read","eadr"),true);
+    expect(hammingDistance("",""),true);
+  });
 }
